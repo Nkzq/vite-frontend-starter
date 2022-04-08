e3bc7def96e8c0051a2ba5a9f4cf44b2355d8623
Romain Lefort <romain.lefort@goandup.fr>
2022-04-08 17:06:55 +0200

    feat(Cleanup)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -15,50 +15,36 @@ import '../scss/app.scss'
 document.documentElement.className = 'js'
 
 conditioner.addPlugin({
-  // the plugin "monitor" hook
-  // use in html : <div data-module="component" data-context="@visible true"></div>
   monitor: {
-    // the name of our monitor, not prefixed with "@"
     name: 'visible',
-    // the monitor factory method, this will create our monitor
     create: (context, element) => ({
-      // current match state
       matches: false,
-
-      // called by conditioner to start listening for changes
       addListener(change) {
         new IntersectionObserver((entries) => {
-          // update the matches state
           this.matches = entries.pop().isIntersecting === context;
-          // inform conditioner of the new state
           change();
         }).observe(element)
       },
     }),
   },
 
-  // converts module aliases to paths
   moduleSetName: (name) => `./modules/${name}.js`,
-
-  // get the module constructor
   moduleGetConstructor: (module) => module.default,
-
-  // override the import (this makes webpack bundle all the dynamically included files as well)
-  // https://webpack.js.org/api/module-methods/#import-
-  // - set to "eager" to create a single chunk for all modules
-  // - set to "lazy" to create a separate chunk for each module
   moduleImport: (name) => import(/* @vite-ignore */ /* webpackMode: "lazy" */ `${name}`),
 })
 
 conditioner.hydrate(document.documentElement)
 
 /**
+ * Due to LazySizes dynamic src
+ * We have to import all images to get them built
+ */
+const images = import.meta.globEager('../images/*')
+
+/**
  * LazySizes configuration
  * https://github.com/aFarkas/lazysizes/#js-api---options
  */
-
-const images = import.meta.globEager('../images/*')
-
 lazySizes.cfg.nativeLoading = {
   setLoadingAttribute: false,
 }
